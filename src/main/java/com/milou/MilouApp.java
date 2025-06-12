185a33e6549f7c2cf8436cc3f2f6dd87f527441a
Macsaroo <sajjad.sarookhani@gmail.com>
2025-06-12 14:00:01 +0330

    write user class version 1 and some basic test and some changes to other class to adapt and test user class ver1

--- src/main/java/com/milou/MilouApp.java
+++ src/main/java/com/milou/MilouApp.java
@@ -1,12 +1,31 @@
 package com.milou;
 
+import com.milou.dao.UserDao;
+import com.milou.models.Email;
+import com.milou.services.AuthService;
+import com.milou.utils.HibernateUtil;
+import com.milou.models.User;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+
+import java.util.Optional;
+
 public class MilouApp {
     public static void main(String[] args) {
-        SessionFactory sessionFactory = new Configuration()
-                .configure("hibernate.cfg.xml")
-                .buildSessionFactory();
-        sessionFactory.close();
+
+        AuthService auth = new AuthService();
+        try {
+
+            User user  = auth.login("testuser@milou.com", "securepassword123");
+            if(user != null) {
+                for(Email email : user.getSentEmails()){
+                    System.out.println(email.getSubject());
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
--- src/main/java/com/milou/dao/UserDao.java
+++ src/main/java/com/milou/dao/UserDao.java
@@ -4,14 +4,16 @@ import com.milou.models.User;
 import com.milou.utils.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+
 import java.util.Optional;
 
 public class UserDao {
+
     public void save(User user) {
         Transaction transaction = null;
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             transaction = session.beginTransaction();
-            session.persist(user); // Use persist for new entities
+            session.persist(user);
             transaction.commit();
         } catch (Exception e) {
             if (transaction != null) {
@@ -21,12 +23,14 @@ public class UserDao {
         }
     }
 
-    public Optional<User> findByEmail(String email) {
+    public User findByEmail(String email) {
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            User user = session.createQuery("FROM User WHERE email = :email", User.class)
+            return session.createQuery("FROM User WHERE email = :email", User.class)
                     .setParameter("email", email)
                     .uniqueResult();
-            return Optional.ofNullable(user);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
         }
     }
 }
