f505db7bc99b2785c194598352d7887918f021f4
Macsaroo <sajjad.sarookhani@gmail.com>
2025-06-12 14:47:49 +0330

    add code generator logic and fix some bugs on saving data to database

---
+++ src/main/java/com/milou/utils/CodeGenerator.java
@@ -0,0 +1,13 @@
+package com.milou.utils;
+import com.milou.dao.EmailDao;
+
+public class CodeGenerator {
+    private static final EmailDao emailDao = new EmailDao();
+    private static int count = emailDao.count();
+
+    public static String generate() {
+        count++;
+        return "msg-"+ emailDao.findMaxId() + count + (int)(Math.random()*10);
+    }
+
+}
