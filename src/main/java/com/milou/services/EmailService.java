ece80593483831170cfdc62a6be7bdc6f62aedf2
Macsaroo <sajjad.sarookhani@gmail.com>
2025-06-14 19:36:51 +0330

    Added mechanism for replying to emails Minor bug fixes

--- src/main/java/com/milou/services/EmailService.java
+++ src/main/java/com/milou/services/EmailService.java
@@ -11,14 +11,18 @@ public class EmailService {
     private final EmailDao emailDao = new EmailDao();
 
 
-    public void sendEmail(Email email) throws Exception {
+    public void sendEmail(Email email, Optional<Email> oldMail) throws Exception {
         if (emailDao.findByCode(email.getMessage_code()) != null) {
             throw new Exception("this email already sent");
         }
 
         try {
             emailDao.save(email);
-            System.out.println("Successfully sent your email.");
+            if (oldMail.isPresent()) {
+                System.out.println("Successfully sent your reply to email " + oldMail.get().getMessage_code() + ".");
+            } else {
+                System.out.println("Successfully sent your email.");
+            }
             System.out.println("Code: " + email.getMessage_code() + "\n");
         } catch (Exception e) {
 
@@ -26,14 +30,15 @@ public class EmailService {
 
     }
 
+
     public static List<Email> allMails(Long userId) {
         EmailDao emailDao = new EmailDao();
         return emailDao.findReceivedEmailsByUserId(userId);
     }
 
-    public static Optional<Email> readByCode(Long userId , String code) {
+    public static Optional<Email> readByCode(Long userId, String code) {
         EmailDao emailDao = new EmailDao();
-        return emailDao.findEmailByCodeForUser(userId , code);
+        return emailDao.findEmailByCodeForUser(userId, code);
     }
 
     public static List<Email> unreadMails(Long userId) {
